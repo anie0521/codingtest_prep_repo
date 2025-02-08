8c1e4173ec01e4d8001460cea4fce157572e0a53
Jisu <jisu.kim0521@gmail.com>
2025-02-08 18:57:49 +0900

    add comments

--- baekjoon_coding/1051.py
+++ baekjoon_coding/1051.py
@@ -13,9 +13,13 @@ size = 1
 while size <= min(N, M):
     for i in range(N - size + 1):
         for j in range(M - size + 1):
+            # 기준점(왼쪽)을 기준으로 같은 줄 오른쪽
             if (map[i][j] == map[i][j + size - 1] and
+                # 왼쪽 아래
                 map[i][j] == map[i + size - 1][j] and
+                # 오른쪽 아래 모서리 값 확인
                 map[i][j] == map[i + size - 1][j + size - 1]):
+                
                 max_square = max(max_square, size * size)
     
     size += 1
