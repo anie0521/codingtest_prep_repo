d71349a44b2af7834534af5949a2306d211c5ff8
Jisu <jisu.kim0521@gmail.com>
2025-02-24 20:36:19 +0900

    testing

--- baekjoon_coding/2579.py
+++ baekjoon_coding/2579.py
@@ -3,6 +3,7 @@ import sys
 n = int(sys.stdin.readline())
 
 stairs = [int(sys.stdin.readline().strip()) for _ in range(n)]
+# [10, 20, 15, 25, 10, 20]
 
 dp = [0] * n
 
@@ -10,10 +11,12 @@ if n <= 2:
     print(sum(stairs))
 
 else:
-    dp[0] = stairs[0]
-    dp[1] = stairs[0] + stairs[1]
+    dp[0] = stairs[0]       #10
+    dp[1] = stairs[0] + stairs[1]   #30
 
     for i in range(2, n):
-        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i] )
+        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i])
+
+        # dp[2] = max(dp[0] + stairs[2], dp[1] + stairs[1] + stairs[2])
     
     print(dp[-1])
