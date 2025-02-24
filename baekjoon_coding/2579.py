c631ca3dcbba5f1746538c9e0159ba23d558e2fa
Jisu <jisu.kim0521@gmail.com>
2025-02-24 20:24:57 +0900

    updated the logic

--- baekjoon_coding/2579.py
+++ baekjoon_coding/2579.py
@@ -6,18 +6,14 @@ stairs = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
 dp = [0] * n
 
-if n == 1:
-    print(stairs[0])
-
-if n == 2:
-    print(stairs[1])
+if n <= 2:
+    print(sum(stairs))
 
 else:
     dp[0] = stairs[0]
     dp[1] = stairs[0] + stairs[1]
-    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
 
-    for i in range(3, n):
-        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
+    for i in range(2, n):
+        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i] )
     
     print(dp[-1])
