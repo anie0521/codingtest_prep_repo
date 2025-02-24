fc23813faf94fe56e27af62257b79347c436a089
Jisu <jisu.kim0521@gmail.com>
2025-02-24 20:48:36 +0900

    updated the for loops

--- baekjoon_coding/2579.py
+++ baekjoon_coding/2579.py
@@ -13,10 +13,9 @@ if n <= 2:
 else:
     dp[0] = stairs[0]       #10
     dp[1] = stairs[0] + stairs[1]   #30
+    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
 
-    for i in range(2, n):
+    for i in range(3, n):
         dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i])
-
-        # dp[2] = max(dp[0] + stairs[2], dp[1] + stairs[1] + stairs[2])
     
     print(dp[-1])
