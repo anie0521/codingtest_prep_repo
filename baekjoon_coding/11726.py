2628a65a2286e31765d0fd25805b16b84f5ce89c
Jisu <jisu.kim0521@gmail.com>
2025-02-26 18:06:20 +0900

    solved 11726

---
+++ baekjoon_coding/11726.py
@@ -0,0 +1,20 @@
+import sys
+
+def ways_of_tiling(n):
+    if n < 1 or n > 1000:
+        return 
+    
+    dp = [0] * (n+1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1007
+
+    return dp[n]
+
+
+n = int(sys.stdin.readline())
+
+print(ways_of_tiling(n))
